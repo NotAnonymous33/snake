cf0bff6a0893287b2cb6076293f8b6cf5cbe5057
Ismail Choudhury <ismail.choudhury33@gmail.com>
2021-07-07 20:02:52 +0100

    add drawing head

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from constants import *
 import pygame
+from snake import Cell, Head
 
 
 pygame.init()
@@ -7,11 +8,34 @@ pygame.init()
 class Game:
     def __init__(self):
         self.window = pygame.display.set_mode((HEIGHT, HEIGHT))
+        self.window.fill((240, 240, 240))
         self.clock = pygame.time.Clock()
+        self.cells = []
+        self.head = Head(self.window)
+        self.game_on = True
 
     def play(self):
+        while self.game_on:
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.game_on = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.head.direction = 0
+                    elif event.key == pygame.K_UP:
+                        self.head.direction = 1
+                    elif event.key == pygame.K_LEFT:
+                        self.head.direction = 2
+                    elif event.key == pygame.K_DOWN:
+                        self.head.direction = 3
+
+            self.head.draw()
+            for cell in self.cells:
+                cell.draw()
 
 
 
 
-        self.clock.tick(FPS)
+            self.clock.tick(FPS)
+
--- snake.py
+++ snake.py
@@ -1,5 +1,29 @@
+import pygame
+from constants import *
+
+
 class Cell:
-    pass
+    def __init__(self, x, y, window):
+        self.x = x
+        self.y = y
+        self.window = window
+
+    def draw(self):
+        pygame.draw.rect(self.window, (0, 0, 0), [self.x*(LENGTH+OFFSET), self.y*(LENGTH+OFFSET), LENGTH, LENGTH])
+
 
 class Head(Cell):
-    pass
+    def __init__(self, window):
+        super().__init__(NUM_ROWS // 2, NUM_ROWS // 2, window)
+        self.direction = 0
+
+    def draw(self):
+        if self.direction == 0:
+            self.x += 1
+        elif self.direction == 1:
+            self.y -= 1
+        elif self.direction == 2:
+            self.x -= 1
+        else:
+            self.y += 1
+        super().draw()
